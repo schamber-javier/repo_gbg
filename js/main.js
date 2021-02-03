345425045bc699618eaa5a13a3fcf4599065a42a
schamber-javier <schamber.javier@gmail.com>
2021-02-03 11:23:32 -0300

    Update 1.1

--- js/main.js
+++ js/main.js
@@ -366,20 +366,21 @@ jQuery(document).ready(function($) {
 	}
 	stickyFillInit();
 
-  var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
 });
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
 });
